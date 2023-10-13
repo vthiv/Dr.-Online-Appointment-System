04c49d085a4aa695676a57914781a19b94164aa2
Thivyah Vijayan <tv9472u@gre.ac.uk>
2023-10-14 01:33:56 +0800

    INDEX DOCTOR are changing

--- doctor/index_doctor.php
+++ doctor/index_doctor.php
@@ -10,15 +10,37 @@ if(isset($_SESSION["user"])){
     header('Location:../index.php');  // Redirecting To Home Page
 }
 
+// Execute SQL queries to get the data
+$queryAppointments = "SELECT COUNT(*) AS totalAppointments FROM appointment WHERE App_Status = 1";
+$queryNewPatients = "SELECT COUNT(*) AS totalNewPatients FROM patient WHERE Created_At >= DATE_SUB(CURDATE(), INTERVAL 30 DAY)";
+$queryTotalDoctors = "SELECT COUNT(*) AS totalDoctors FROM doctor";
+
+$resultAppointments = mysqli_query($connection, $queryAppointments);
+$resultNewPatients = mysqli_query($connection, $queryNewPatients);
+$resultTotalDoctors = mysqli_query($connection, $queryTotalDoctors);
+
+if ($resultAppointments && $resultNewPatients && $resultTotalDoctors) {
+    $dataAppointments = mysqli_fetch_assoc($resultAppointments);
+    $dataNewPatients = mysqli_fetch_assoc($resultNewPatients);
+    $dataTotalDoctors = mysqli_fetch_assoc($resultTotalDoctors);
+
+    $totalAppointments = $dataAppointments['totalAppointments'];
+    $totalNewPatients = $dataNewPatients['totalNewPatients'];
+    $totalDoctors = $dataTotalDoctors['totalDoctors'];
+} else {
+    // Handle database query errors here
+}
+
 //Retrieve the doctor's name
 $doctorEmail = $_SESSION["user"]; //Assuming store the doctor's email in the session
-$query = "SELECT `Doctor_Name`, `Dept_ID` FROM `doctor` WHERE `Email` = '$doctorEmail'";
-$result=mysqli_query($connection,$query);
+$query = "SELECT `Doctor_Name`, `Dept_ID`, `Profile_Image` FROM `doctor` WHERE `Email` = '$doctorEmail'";
+$result = mysqli_query($connection, $query);
 
 if ($result && mysqli_num_rows($result) > 0) {
     $doctorData = mysqli_fetch_assoc($result);
     $doctorName = $doctorData['Doctor_Name'];
     $deptID = $doctorData['Dept_ID'];
+    $profileImage = $doctorData['Profile_Image'];
 
     // Fetch the department name based on the department ID
     $deptQuery = "SELECT `Dept_Name` FROM `department` WHERE `Dept_ID` = $deptID";
@@ -27,18 +49,31 @@ if ($result && mysqli_num_rows($result) > 0) {
     if ($deptResult && mysqli_num_rows($deptResult) > 0) {
         $deptData = mysqli_fetch_assoc($deptResult);
         $departmentName = $deptData['Dept_Name'];
-    }
-    else {
+    } else {
         $departmentName = "Unknown Department"; // Default if department not found
     }
-}
-else {
+} else {
     $doctorName = "Doctor"; // Default name if not found
     $departmentName = "Unknown Department"; // Default department if not found
+    $profileImage = "default.jpg"; // Default image if not found
 }
 
-// Generate the image source based on the doctor's name
-$profileImage = strtolower(str_replace(' ', '_', $doctorName)) . '.jpg'; // Assumes lowercase and underscores in filenames
+
+// Fetch all doctors grouped by department
+$doctorsByDept = [];
+$doctorsQuery = "SELECT d.Doctor_Name, d.Profile_Image, d.Dept_ID, dp.Dept_Name FROM doctor d 
+                INNER JOIN department dp ON d.Dept_ID = dp.Dept_ID";
+$resultDoctors = mysqli_query($connection, $doctorsQuery);
+
+if ($resultDoctors && mysqli_num_rows($resultDoctors) > 0) {
+    while ($row = mysqli_fetch_assoc($resultDoctors)) {
+        $doctorsByDept[$row['Dept_Name']][] = [
+            'name' => $row['Doctor_Name'],
+            'profileImage' => $row['Profile_Image']
+        ];
+    }
+}
+
 
 ?>
 
@@ -112,7 +147,7 @@ $profileImage = strtolower(str_replace(' ', '_', $doctorName)) . '.jpg'; // Assu
                     <div class="text-center avatar-profile margin-nagative mt-n5 position-relative pb-2 border-0">
                         <img src="../img/doctors/<?php echo $profileImage; ?>" class="rounded-circle shadow-md avatar avatar-md-md" />
                         <h5 class="mt-3 mb-1"><?php echo $doctorName; ?></h5>
-                        <p class="text-muted mb-0">Doctor <?php echo $departmentName; ?></p>
+                        <p class="text-muted mb-0">Doctor in <?php echo $departmentName; ?></p>
                     </div>
                 </div>
                 <li class="nav-item">
@@ -177,13 +212,13 @@ $profileImage = strtolower(str_replace(' ', '_', $doctorName)) . '.jpg'; // Assu
                             <div class="col-xxl-4 col-md-6">
                                 <div class="card info-card appointment-card">
                                     <div class="card-body">
-                                        <h5 class="card-title">New Appointments</h5>
+                                        <h5 class="card-title">Appointments</h5>
                                         <div class="d-flex align-items-center">
                                             <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                                                 <i class="bi bi-calendar-check-fill"></i>
                                             </div>
                                             <div class="ps-3">
-                                                <h5 class="mb-0">Data</h5>
+                                                <h5 class="mb-0"><?php echo $totalAppointments; ?></h5>
                                             </div>
                                         </div>
                                     </div>
@@ -195,13 +230,13 @@ $profileImage = strtolower(str_replace(' ', '_', $doctorName)) . '.jpg'; // Assu
                             <div class="col-xxl-4 col-md-6">
                                 <div class="card info-card patient-card">
                                     <div class="card-body">
-                                        <h5 class="card-title">New Patients</h5>
+                                        <h5 class="card-title">Patients</h5>
                                         <div class="d-flex align-items-center">
                                             <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                                                 <i class="bi bi-people"></i>
                                             </div>
                                             <div class="ps-3">
-                                                <h5 class="mb-0">Data</h5>
+                                                <h5 class="mb-0"><?php echo $totalNewPatients; ?></h5>
                                             </div>
                                         </div>
                                     </div>
@@ -209,7 +244,7 @@ $profileImage = strtolower(str_replace(' ', '_', $doctorName)) . '.jpg'; // Assu
                             </div>
                             <!--No of Patients Card Ends -->
 
-                            <!--No of Appointment Card -->
+                            <!--No of Doctor Card -->
                             <div class="col-xxl-4 col-md-6">
                                 <div class="card info-card doctor-card">
                                     <div class="card-body">
@@ -219,13 +254,13 @@ $profileImage = strtolower(str_replace(' ', '_', $doctorName)) . '.jpg'; // Assu
                                                 <i class="bi bi-heart-pulse"></i>
                                             </div>
                                             <div class="ps-3">
-                                                <h5 class="mb-0">Data</h5>
+                                                <h5 class="mb-0"><?php echo $totalDoctors; ?></h5>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <!--No of Appointment Card Ends -->
+                            <!--No of Doctor Card Ends -->
 
                             <!--Appointment Table -->
                             <div class="col-12">
@@ -233,7 +268,7 @@ $profileImage = strtolower(str_replace(' ', '_', $doctorName)) . '.jpg'; // Assu
                                     <div class="card-header">
                                         <h4 class="card-title">
                                             Appointment List
-                                            <a href="appointment_admin.php" style="float: right;"><i class="bi bi-grid"></i></a>
+                                            <a href="appointment_doctor.php" style="float: right;"><i class="bi bi-grid"></i></a>
                                         </h4>
                                     </div>
                                     <div class="card-block">
@@ -250,14 +285,42 @@ $profileImage = strtolower(str_replace(' ', '_', $doctorName)) . '.jpg'; // Assu
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    <tr>
-                                                        <td>Appointment ID</td>
-                                                        <td>Patient Name</td>
-                                                        <td>Doctor Name</td>
-                                                        <td>Appointment Date</td>
-                                                        <td>Appointment Time</td>
-                                                        <td>Appointment Status</td>
-                                                    </tr>
+                                                <?php
+                                                    // Execute SQL query to get appointment data
+                                                    $queryAppointmentsData = "SELECT
+                                                        appointment.App_ID,
+                                                        CONCAT(patient.Pat_Firstname, ' ', patient.Pat_Lastname) AS patient_name,
+                                                        doctor.Doctor_Name,
+                                                        appointment.App_Date,
+                                                        appointment.App_Time,
+                                                        appointment.App_Status
+                                                    FROM
+                                                        appointment
+                                                    INNER JOIN
+                                                        patient ON appointment.Pat_ID = patient.Pat_ID
+                                                    INNER JOIN
+                                                        doctor ON appointment.Doctor_ID = doctor.Doctor_ID
+                                                    WHERE
+                                                        appointment.App_Status = 1"; // Assuming 1 is for active appointments
+
+                                                    $resultAppointmentsData = mysqli_query($connection, $queryAppointmentsData);
+
+                                                    if ($resultAppointmentsData && mysqli_num_rows($resultAppointmentsData) > 0) {
+                                                        // Loop through the appointment data and display it in the table
+                                                        while ($row = mysqli_fetch_assoc($resultAppointmentsData)) {
+                                                            echo '<tr>';
+                                                            echo '<td>' . $row['App_ID'] . '</td>';
+                                                            echo '<td>' . $row['patient_name'] . '</td>';
+                                                            echo '<td>' . $row['Doctor_Name'] . '</td>';
+                                                            echo '<td>' . $row['App_Date'] . '</td>';
+                                                            echo '<td>' . $row['App_Time'] . '</td>';
+                                                            echo '<td>' . ($row['App_Status'] == 1 ? 'Active' : 'Inactive') . '</td>';
+                                                            echo '</tr>';
+                                                        }
+                                                    } else {
+                                                        echo '<tr><td colspan="6">No appointments found.</td></tr>';
+                                                    } 
+                                                    ?>
                                                 </tbody>
                                             </table>
                                         </div>
@@ -271,28 +334,47 @@ $profileImage = strtolower(str_replace(' ', '_', $doctorName)) . '.jpg'; // Assu
 
                     <!-- Right side columns -->
                     <div class="col-lg-4">
-                            <div class="card member-panel">
-                                <div class="card-header bg-white">
-                                    <h4 class="card-title">Doctors
-                                        <a href="doctors_admin.php" style="float: right;"><i class="bi bi-grid"></i></a>
-                                    </h4>
-                                </div>
-                                <div class="card-body">
-                                    <ul class="contact-list">
-                                        <li>
-                                            <div class="contact-cont">
-                                                <div class="float-left user-img m-r-10">
-                                                    <a href="#"><img src="#" class="w-40 rounded-circle"/></a>
-                                                </div>
-                                                <div class="contact-info">
-                                                    <span class="contact-name text-ellipsis">Name</span>
-                                                    <span class="contact-date">Speciality</span>
-                                                </div>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
+                        <div class="card member-panel">
+                            <div class="card-header bg-white">
+                                <h4 class="card-title">Doctors
+                                    <a href="doctors_admin.php" style="float: right;"><i class="bi bi-grid"></i></a>
+                                </h4>
                             </div>
+                            <div class="card-body">
+                                <ul class="contact-list">
+                                <?php
+                                foreach ($doctorsByDept as $deptName => $doctors) {
+                                    foreach ($doctors as $doctor) {
+                                        echo '<li>
+                                                <div class="contact-cont d-flex align-items-center">
+                                                    <div class="float-left user-img m-r-10" style="margin-right: 5px;">
+                                                        <a href="#"><img src="../img/doctors/' . $doctor['profileImage'] . '" class="w-40 rounded-circle" style="max-width: 40px; max-height: 40px;"/></a>
+                                                    </div>
+                                                    <div class="contact-info">
+                                                        <span class="contact-name text-ellipsis">' . $doctor['name'] . '</span>
+                                                        <span class="contact-date">' . $deptName . '</span>
+                                                    </div>
+                                                </div>
+                                            </li>';
+                                    }
+                                }
+                                ?>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-lg-4">
+                        <div class="card member-panel">
+                            <div class="card-header bg-white">
+                                <h4 class="card-title">Doctor Schedule
+                                    <a href="doctors_admin.php" style="float: right;"><i class="bi bi-grid"></i></a>
+                                </h4>
+                            </div>
+                            <div class="card-body">
+                                
+                            </div>
+                        </div>
                     </div>
                     <!-- End Right side columns -->
                 </div>
